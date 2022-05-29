35cfd63335389a066e3b5cc6b9bf2d961125b5e5
clipssender <100996857+clipssender31@users.noreply.github.com>
2022-05-29 12:18:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def GetProxy():
 def register():
     while True:
         try:
-            ll = "github.com/clipssender | ".join(random.choice(string.ascii_lowercase + string.digits) for i in range(8))
+            ll = "".join(random.choice(string.ascii_lowercase + string.digits) for i in range(8))
             Email=f"{ll}@outlook.com"
             Username=f"{ll}"
             client = httpx.Client(headers = {"authority": "www.guilded.gg","method": "POST", "path": "/api/users?type=email", "scheme": "https", "accept": "application/json, text/javascript, */*; q=0.01", "accept-encoding": "gzip, deflate, br","accept-language": "en-GB,en-US;q=0.9,en;q=0.8", "content-type": "application/json", "guilded-client-id": f"{uuid.uuid1()}","guilded-device-id": str(binascii.b2a_hex(os.urandom(64)).decode("utf-8")), "guilded-device-type": "desktop", "guilded-stag": "c4740afd3f3e4d63d365d826139de166", "origin": "https://www.guilded.gg", "referer": "https://www.guilded.gg/","sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36", "x-requested-with": "XMLHttpRequest", "dnt": "1","Sec-Ch-Ua": '" Not;A Brand";v="99", '"Google Chrome;v=97"', '"Chromium"';v=97',"Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "macOS" } ,proxies=GetProxy())
@@ -29,7 +29,7 @@ def register():
                 with open("Accounts.txt", "a+") as tk:
                     tk.write(f"{Email}:qpwo12!0-94-23_#$_#$@:{Hmac}\n")
             else:
-                print(RegisterReq)
+                print(RegisterReq.json())
                 register()
         except Exception as error:
             print(error)
